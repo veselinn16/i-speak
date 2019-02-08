240fbf7adcda20f88c66c68cddbee6fe3430cb9c
veselinn16 <veselinn16@gmail.com>
2019-02-08 12:15:29 +0200

    Subscribe for updates to Redux store :wrench:

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderer = () => {
+    fancyLog();
+    return ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+renderer();
+
+store.subscribe(renderer);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function fancyLog() {
+    console.log("%c Rendered with � � � ", "background: purple; color: #FFF");
+    console.log(store.getState());
+}
