5dc4d60d965c52af6a4bbfc03eecdb0868772ccc
veselinn16 <veselinn16@gmail.com>
2019-02-08 13:39:30 +0200

    Pass users to User component as props :wrench:

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Main from './Main';
 import store from './store';
+import _ from 'lodash';
 
 const App = () => {
-  const contacts = store.getState();
+  const contacts = store.getState().contacts;
 
   return (
     <div className="App">
-      <Sidebar contacts={contacts}/>
+      <Sidebar contacts={_.values(contacts)}/>
       <Main/>
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import User from './User';
 
-const Sidebar = () => {
-    return <aside className="sidebar">Sidebar</aside>
+const Sidebar = ({ contacts }) => {
+    return <aside className="sidebar">
+                {contacts.map(contact => <User user={contact} key={contact.user_id}/>)}
+            </aside>
 }
 
 export default Sidebar;
