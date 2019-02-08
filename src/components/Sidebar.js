ac5ea45071dc37bce2cc5cd34353d07b59d9b008
veselinn16 <veselinn16@gmail.com>
2019-02-08 20:58:39 +0200

    Separate components into own folder :recycle:

---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import User from '../containers/User';
+
+const Sidebar = ({ contacts }) => {
+    return <aside className="sidebar">
+                {contacts.map(contact => <User user={contact} key={contact.user_id}/>)}
+            </aside>
+}
+
+export default Sidebar;
