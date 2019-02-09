70741811c8b9d53a2884ef0e39918e47e9ae6744
veselinn16 <veselinn16@gmail.com>
2019-02-09 21:36:25 +0200

    Render all child components of ChatWindow :wrench:

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import _ from 'lodash';
+
 import store from '../store';
+
 import Header from '../components/Header';
+import Messages from '../components/Messages';
+import MessageInput from './MessageInput';
+
 
 const ChatWindow = ({activeUserId}) => { // gets object.activeUserId
     const state = store.getState();
     const activeUser = state.contacts[activeUserId]; // gets user from state.contacts via activeUserId
+    const userMessages = state.messages[activeUserId]; // gets messages of the selected user
+    const inputValue = state.typing;
 
     return (
         <div className='chat-window'>
             <Header user={activeUser} />
+            <Messages messages={_.values(userMessages)} />
+            <MessageInput value={inputValue} />
         </div>
     );
 };
