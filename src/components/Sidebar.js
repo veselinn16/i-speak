0495ed83e218480631c338a542aa6afb33369aa7
veselinn16 <veselinn16@gmail.com>
2019-02-09 11:57:27 +0200

    Create layout for Sidebar component :construction:

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,9 @@ import User from '../containers/User';
 
 const Sidebar = ({ contacts }) => {
     return <aside className="sidebar">
-                {contacts.map(contact => <User user={contact} key={contact.user_id}/>)}
+                {contacts.map(contact => {
+                // contact is a user object
+                return <User user={contact} key={contact.user_id}/>})}
             </aside>
 }
 
