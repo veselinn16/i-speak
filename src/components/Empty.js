0ccd3e85916944ed2f643dd6a91801d0a321f490
veselinn16 <veselinn16@gmail.com>
2019-02-09 11:56:46 +0200

    Create layout for Empty component :construction:

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+const Empty = ({ user }) => {
+    const { name, profile_pic, status } = user;
+    const firstName = name.split(' ')[0];
+
+    return(
+        <div className="empty">
+            <h1 className="empty-name">Welcome, {firstName} </h1>
+            <img src={profile_pic} alt={name} className="empty-img" />
+            <p className="empty-status">
+                <b>Status:</b> {status}
+            </p>
+            <button className="empty-btn">Start a conversation</button>
+            <p className="empty-info">
+                Search for someone to start chatting with or go to Contacts
+                to see who
+                is available
+            </p>
+        </div>
+    )
+}
+
+export default Empty;
